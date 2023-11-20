4fb8ccfbc5e6f5633fb0f790ac5f2c5bfc0e1625
rocknro1l <alexst383@gmail.com>
2023-11-20 09:33:14 +0100

    test1

--- src/js/fetchData.js
+++ src/js/fetchData.js
@@ -1,13 +1,14 @@
 import axios from 'axios';
 
+axios.defaults.baseURL = 'https://pixabay.com/api/';
 const API_KEY = '40692232-4084d53797a695134229ef5d4';
-const BASE_URL = 'https://pixabay.com/api/';
 const per_page = 40;
 
 async function fetchData(query, currentPage) {
-  const url = `${BASE_URL}?key=${API_KEY}&q=${query}&image_type=photo&pretty=true&per_page=${per_page}&page=${currentPage}&orientation=horizontal&safesearch=true`;
+  const url = `?key=${API_KEY}&q=${query}&image_type=photo&pretty=true&per_page=${per_page}&page=${currentPage}&orientation=horizontal&safesearch=true`;
   try {
     const { data } = await axios.get(url);
+    console.log('data:', data);
 
     return data;
   } catch (error) {
